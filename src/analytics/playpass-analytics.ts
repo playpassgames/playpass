bdf5229e0ebc475fe9e4deea76cd5be67062a401
Jake Davies <jgdavies24@gmail.com>
2022-04-21 14:04:09 +1000

    Track referrer and page by default

--- src/analytics/playpass-analytics.ts
+++ src/analytics/playpass-analytics.ts
@@ -74,12 +74,17 @@ export class PlaypassAnalytics implements Analytics {
             setTimeout(() => { this.flush() }, SEND_DELAY);
         }
 
+        const defaultEventProperties = {
+          page: document.location.href,
+          referrer: document.referrer,
+        }
+
         this.eventQueue.push({
             user_id: getPlayerId(),
             session_id: this.sessionId,
             event_time: new Date().toISOString(),
             event_type: name,
-            event_properties: props || {},
+            event_properties: {...defaultEventProperties, ...(props || {})},
             user_properties: this.userProps,
         });
     }
