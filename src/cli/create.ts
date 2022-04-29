a80bfa8035799e65c6fd83f2d4bbeab168b1aa9f
Bruno Garcia <b@aduros.com>
2022-04-29 14:23:38 -0300

    Have `playpass create` handle existing projects, and update docs. (#15)
    If you run `playpass create` in an existing project with a package.json,
    you'll be prompted to add Playpass to it instead of creating from a
    template.
    
    Choosing "yes" will reserve a game ID, generate a `playpass.toml`, and
    print out some instructions of how to modify your game JS to init
    playpass.

--- src/cli/create.ts
+++ src/cli/create.ts
@@ -26,8 +26,20 @@ async function prompt (obj: Partial<PromptObject>): Promise<string> {
     return value;
 }
 
+async function exists (file: string): Promise<boolean> {
+    try {
+        await fs.stat(file);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 export async function create (destDir: string | undefined, opts: { template?: string }): Promise<void> {
     let subdomain: string;
+
+    // Whether we're creating using a template, or in-place adding Playpass to an existing project
+    let useTemplate = true;
 
     if (!destDir) {
         // No directory param provided, prompt them for a project ID and use it to form the dest dir
@@ -36,7 +48,16 @@ export async function create (destDir: string | undefined, opts: { template?: st
             message: "What will we call your project?",
             initial: "my-game",
         }));
-        destDir = path.resolve(subdomain);
+
+        if (await exists(process.cwd()+"/package.json")) {
+            useTemplate = !await prompt({
+                type: "confirm",
+                message: "This directory already contains a package.json, should we use it as an existing project?",
+                initial: true,
+            });
+        }
+
+        destDir = useTemplate ? path.resolve(subdomain) : process.cwd();
 
     } else {
         // They provided a dest dir, infer the project ID from it and make sure it's absolute
@@ -54,6 +75,31 @@ export async function create (destDir: string | undefined, opts: { template?: st
         // Continue
     }
 
+    let template;
+
+    if (useTemplate) {
+        try {
+            await fs.stat(destDir);
+            console.error(`Directory ${destDir} already exists. Please use a different name.`);
+            return;
+        } catch (error) {
+            // Continue
+        }
+
+        template = opts.template || await prompt({
+            type: "select",
+            name: "template",
+            message: "Choose a project template below",
+            choices: [
+                { title: "Daily Level Game", value: "daily-level" },
+                { title: "React Daily Level Game", value: "react-daily-level" },
+                { title: "Daily Word Game", value: "word-daily-level" },
+            ],
+        });
+    }
+
+    console.log();
+
     let game: Game;
     try {
         game = await playpassClient.create(subdomain);
@@ -61,49 +107,30 @@ export async function create (destDir: string | undefined, opts: { template?: st
         throw new Error(`Failed to create game ${subdomain}, please try again`);
     }
 
-    const template = opts.template || await prompt({
-        type: "select",
-        name: "template",
-        message: "Choose a project template below",
-        choices: [
-            { title: "Daily Level Game", value: "daily-level" },
-            { title: "React Daily Level Game", value: "react-daily-level" },
-            { title: "Daily Word Game", value: "word-daily-level" },
-        ],
-    });
+    if (useTemplate) {
+        const transform = (src: string, dest: string) => {
+            const ext = path.extname(src);
+            if (ext != ".js" && ext != ".jsx" && ext != ".ts" && ext != ".tsx") {
+                return null;
+            }
+            return replace("YOUR_GAME_ID", game.id);
+        };
 
-    console.log();
+        const templatesDir = `${__dirname}/../../../templates`;
+        await copy(`${templatesDir}/${template}`, destDir, { dot: true, junk: true, transform });
+        await copy(`${templatesDir}/common`, destDir, { dot: true, junk: true });
 
-    try {
-        await fs.stat(destDir);
-        console.error(`Directory ${destDir} already exists. Please use a different name.`);
-        return;
-    } catch (error) {
-        // Continue
+        // Update package.json. This is only to have a sensible default and not used by Playpass
+        const json = JSON.parse(await fs.readFile(destDir+"/package.json", "utf8"));
+        json.name = subdomain;
+        await fs.writeFile(destDir+"/package.json", JSON.stringify(json, null, "  "));
     }
-
-    const transform = (src: string, dest: string) => {
-        const ext = path.extname(src);
-        if (ext != ".js" && ext != ".jsx" && ext != ".ts" && ext != ".tsx") {
-            return null;
-        }
-        return replace("YOUR_GAME_ID", game.id);
-    };
-
-    const templatesDir = `${__dirname}/../../../templates`;
-    await copy(`${templatesDir}/${template}`, destDir, { dot: true, junk: true, transform });
-    await copy(`${templatesDir}/common`, destDir, { dot: true, junk: true });
 
     // Generate an initial playpass.toml
     await fs.writeFile(destDir+"/playpass.toml", [
         "# Your game's unique identifier. This should never change.",
         `game_id = "${game.id}"`,
     ].join("\n") + "\n");
-
-    // Also, update package.json. This is only to have a sensible default and not used by Playpass
-    const json = JSON.parse(await fs.readFile(destDir+"/package.json", "utf8"));
-    json.name = subdomain;
-    await fs.writeFile(destDir+"/package.json", JSON.stringify(json, null, "  "));
 
     console.log("Installing NPM dependencies, this may take a minute...");
 
@@ -113,18 +140,33 @@ export async function create (destDir: string | undefined, opts: { template?: st
     });
 
     console.log();
-    console.log(`${kleur.green("✔")} Created project at ${destDir}`);
-    console.log();
-    console.log("Develop it by running:");
-    console.log();
-    console.log("    npm start");
-    console.log();
-    console.log("Build for production by running:");
-    console.log();
-    console.log("    npm run build");
-    console.log();
-    console.log("Deploy it by pushing to GitHub, or manually running:");
-    console.log();
-    console.log("    playpass deploy");
-    console.log();
+
+    if (useTemplate) {
+        console.log(`${kleur.green("✔")} Created new project at ${destDir}`);
+        console.log();
+        console.log("Develop it by running:");
+        console.log();
+        console.log("    npm start");
+        console.log();
+        console.log("Build for production by running:");
+        console.log();
+        console.log("    npm run build");
+        console.log();
+        console.log("Deploy it by pushing to GitHub, or manually running:");
+        console.log();
+        console.log("    playpass deploy");
+        console.log();
+
+    } else {
+        console.log(`${kleur.green("✔")} Added Playpass to an existing project at ${destDir}`);
+        console.log();
+        console.log("To start using Playpass, edit your main JS file to add this import:");
+        console.log();
+        console.log("    import * as playpass from \"playpass\";");
+        console.log();
+        console.log("And paste this line into your game's initialization:");
+        console.log();
+        console.log(`    await playpass.init({ gameId: "${game.id}" });`);
+        console.log();
+    }
 }
