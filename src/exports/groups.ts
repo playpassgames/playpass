2a4aed0ae5c5f9565f2a7195b783f86a7f8d30fe
Bruno Garcia <b@aduros.com>
2022-05-03 20:34:20 -0300

    Add groups support. (#18)

--- src/exports/groups.ts
+++ src/exports/groups.ts
@@ -2,4 +2,6 @@
 // Playpass (c) Playco
 // https://github.com/playpassgames/playpass/blob/main/LICENSE.txt
 
-export { login, logout, isLoggedIn, getPlayerId } from "../login";
+export { createGroup, getGroup } from "../groups";
+
+export type { Group } from "../groups";
--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,14 @@ export { getLinkData } from "./links";
 export { init } from "./init";
 export { copyToClipboard, createLink, share } from "./share";
 export { storage } from "./storage";
-// export { createGroup, getGroup } from "./groups";
 export { createFeatureFlag, featureFlagIsEnabled, setFeatureFlagEnabled } from "./featureFlags";
 
 export * as account from "./exports/account";
+export * as groups from "./exports/groups";
 export * as payments from "./exports/payments";
 
 export type { Analytics } from "./analytics";
 export type { FeatureFlagOptions } from "./featureFlags";
-export type { Group } from "./groups";
 export type { InitOptions } from "./init";
 export type { ShareOptions, CreateLinkOptions } from "./share";
 export type { Storage } from "./storage";
