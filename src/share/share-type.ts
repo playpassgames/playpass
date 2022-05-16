c7884f7269129c614887d7a7819413c5a4d35211
Bruno Garcia <b@aduros.com>
2022-05-16 15:41:51 -0300

    Add explicit type param to `playpass.share`. (#28)

---
+++ src/share/share-type.ts
@@ -0,0 +1,38 @@
+//
+// Playpass (c) Playco
+// https://github.com/playpassgames/playpass/blob/main/LICENSE.txt
+
+/**
+* A preferred share type to pass to {@link share}.
+*
+* Example:
+*
+* ```js
+* playpass.share({
+*     text: "Hello world",
+*     type: "twitter",
+* });
+* ```
+*/
+export enum ShareType {
+    /** Allow the player to choose where to share. */
+    Any = "any",
+
+    /** Share as a post on Facebook. */
+    Facebook = "facebook",
+
+    /** Share as a tweet on Twitter. */
+    Twitter = "twitter",
+
+    /** Share as a message on WhatsApp. */
+    WhatsApp = "whatsapp",
+
+    /** Share as a message on Telegram. */
+    Telegram = "telegram",
+
+    /** Share by copying text to the system clipboard. */
+    Clipboard = "clipboard",
+
+    // /** Share as an email. */
+    // Email = "email",
+}
