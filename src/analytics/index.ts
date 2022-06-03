5b3946134b8eff03c9eff02c6fc95178a71cb0a3
Nicholas Hydock <nicholas.hydock@play.co>
2022-06-03 16:42:59 -0400

    remove unused transformer code

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -9,23 +9,14 @@ export type { Analytics, EventProps } from "./analytics";
 
 export const playpassAnalytics = new PlaypassAnalytics();
 
-export type AnalyticsEventTransformer = (name: string, props?: EventProps) => EventProps;
-
 // Sends analytics to both backends during the transition period
 class MirrorAnalytics implements Analytics {
     private targets: Analytics[] = [playpassAnalytics];
 
-    private interceptors: AnalyticsEventTransformer[] = [];
-
     track(name: string, props?: EventProps) {
-        const evt = this.interceptors.reduce(
-            (out, transformer) => transformer(name, out),
-            props
-        );
-
         this.targets.forEach(
             (target) => {
-                target.track(name, evt);
+                target.track(name, props);
             }
         );
     }
@@ -41,10 +32,6 @@ class MirrorAnalytics implements Analytics {
     addTarget(target: Analytics) {
         this.targets.push(target);
     }
-
-    addTransformer(transformer: AnalyticsEventTransformer) {
-        this.interceptors.push(transformer);
-    }
 }
 
 const mirror = new MirrorAnalytics();
@@ -54,8 +41,4 @@ export const analytics: Analytics = mirror;
 
 export function injectSecondaryAnalytics(analytics: Analytics) {
     mirror.addTarget(analytics);
-}
-
-export function injectAnalyticsTransformer(transformer: AnalyticsEventTransformer) {
-    mirror.addTransformer(transformer);
 }
