203b1dfbcf0295003aef2b650540e7ac0e205529
Jake Davies <jgdavies24@gmail.com>
2022-09-23 16:35:10 +1000

    unsubscribe fix

--- src/login.ts
+++ src/login.ts
@@ -182,7 +182,7 @@ export async function initLogin(gameId: string): Promise<void> {
                 },
             },
 
-            version: "1.4.3-alpha2",
+            version: "1.4.3-alpha3",
             endpoint: "https://replicant-lite.us-east-1.replicant-playpass.gc-internal.net/replicant-lite-dev",
         },
         stripeAccountId: getStripeAccount(),
